d4a37696c103a86f4646b475c917b2f7a05b69bb
Simon <simon-courtois@hotmail.com>
2026-05-18 14:21:05 +0200

    [Particle]
    Spawner + Area effector

--- Assets/_/Code/Game/ParticleSpawner/Runtime/Spawner.cs
+++ Assets/_/Code/Game/ParticleSpawner/Runtime/Spawner.cs
@@ -4,9 +4,10 @@ namespace ParticleSpawner.Runtime
 {
     public class Spawner : MonoBehaviour
     {
-        [SerializeField] private float _elapsedTime;
-        [SerializeField] private float _timeBetweenParticle;
-        [SerializeField] private GameObject _particle;
+        private void Update()
+        {
+            SpawnParticle();
+        }
 
         private void SpawnParticle()
         {
@@ -14,7 +15,15 @@ namespace ParticleSpawner.Runtime
             if (_elapsedTime <= 0)
             {
                 _elapsedTime = _timeBetweenParticle;
+                GameObject particle = Instantiate(_particle, transform.position + Random.insideUnitSphere * _spawnerRadius, Quaternion.identity);
+                Destroy(particle, _lifeSpanParticle);
             }
         }
+
+        private float _elapsedTime;
+        [SerializeField] private float _timeBetweenParticle;
+        [SerializeField] private float _spawnerRadius;
+        [SerializeField] private float _lifeSpanParticle;
+        [SerializeField] private GameObject _particle;
     }
 }
