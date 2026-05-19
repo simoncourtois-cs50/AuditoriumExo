98f23b08e5609bcf5a5af51193524fef0c8d8686
Simon Courtois <simon-courtois@hotmail.com>
2026-05-19 23:43:59 +0200

    [Resize]
    Resize + state Machine

--- Assets/_/Code/Game/Controller/Runtime/DragAndDrop.cs
+++ Assets/_/Code/Game/Controller/Runtime/DragAndDrop.cs
@@ -9,37 +9,98 @@ namespace Controller.Runtime
         private void Awake()
         {
             
-            _clickAction = InputSystem.actions.FindAction("Attack");
+            _clickInput = InputSystem.actions.FindAction("Attack");
             _camera = Camera.main;
         }
-       
+        
         private void Update()
         {
-            if (_clickAction.WasPressedThisFrame())
-            {
-                Vector3 mousePosition = GetMousePosition();
-                Vector3 direction = mousePosition - _camera.transform.position;
-
-                Collider2D hit = Physics2D.OverlapPoint(mousePosition);
-
-                if (hit && hit.transform.CompareTag("Deviation"))
-                {
-                    _currentDragableObject = hit.transform.gameObject;
-                }
-            }
-
-            if (_clickAction.WasReleasedThisFrame())
-            {
-                _currentDragableObject = null;
-            }
-            if (_currentDragableObject)
-            {
-                _currentDragableObject.transform.position = GetMousePosition();
-            }
+            OnStayState();
         }
 
         #endregion
 
+
+        #region StateMachine
+
+        private void OnStayState()
+        {
+            switch (_currentState)
+            {
+                case State.Rest:
+                    
+                    if (_clickInput.WasPressedThisFrame())
+                    {
+                        Vector3 mousePosition = GetMousePosition();
+
+                        Collider2D hit = Physics2D.OverlapPoint(mousePosition);
+
+                        if (hit && hit.transform.CompareTag("Deviation"))
+                        {
+                            _currentClickedObject = hit.transform.gameObject;
+                            BoxCollider2D hitCollider = _currentClickedObject.GetComponent<BoxCollider2D>();
+                    
+                            Vector2 width = Vector2.Scale(hitCollider.size, _currentClickedObject.transform.lossyScale);
+                    
+                            if (Vector3.Distance(_currentClickedObject.transform.position, GetMousePosition()) > (width.x * (0.6 * 0.5)))
+                            {
+                                ChangeState(State.Resize);
+                            }
+                            else
+                            {
+                                ChangeState(State.Drag);
+                            }
+                        }
+                    }
+                    break;
+                
+                case State.Drag:
+                    if (_currentClickedObject)
+                    {
+                        _currentClickedObject.transform.position = GetMousePosition();
+                    }
+                    CheckClickRelease();
+                    break;
+                case State.Resize:
+                    Resize();
+                    CheckClickRelease();
+                    break;
+            }
+        }
+        private void OnEnterState()
+        {
+            switch (_currentState)
+            {
+                case State.Rest:
+                    _currentClickedObject = null;
+                    break;
+                case State.Drag:
+                    break;
+                case State.Resize:
+                    break;
+            }
+        }
+        private void OnExitState()
+        {
+            switch (_currentState)
+            {
+                case State.Rest:
+                    break;
+                case State.Drag:
+                    break;
+                case State.Resize:
+                    break;
+            }
+        }
+
+        private void ChangeState(State newState)
+        {
+            OnExitState();
+            _currentState = newState;
+            OnEnterState();
+        }
+
+        #endregion
 
         #region Main API
 
@@ -52,9 +113,19 @@ namespace Controller.Runtime
             return mouseWorldPosition;
 
         }
-        private void ResizeRenderer(GameObject resizableObject)
+
+        private void Resize()
         {
-            resizableObject.transform.localScale *= 2f;
+            _horizontalInput = Mouse.current.delta.x.ReadValue();
+            _currentClickedObject.transform.localScale *= 1 + (_horizontalInput * 0.1f);
+        }
+
+        private void CheckClickRelease()
+        {
+            if (_clickInput.WasReleasedThisFrame())
+            {
+                ChangeState(State.Rest);
+            }
         }
 
         #endregion
@@ -62,12 +133,18 @@ namespace Controller.Runtime
 
         #region Private and Protected
 
-        private InputAction _clickAction;
+        private InputAction _clickInput;
+        private float _horizontalInput;
         private Camera _camera;
-        private GameObject _currentDragableObject;
-        private bool _currentResizableObject;
+        private GameObject _currentClickedObject;
+        private enum State
+        {
+            Rest,
+            Drag,
+            Resize
+        }
 
-        [SerializeField] private GameObject _forceHalo;
+        private State _currentState;
 
         #endregion
     }
